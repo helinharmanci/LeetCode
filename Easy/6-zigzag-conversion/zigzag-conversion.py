39b469b7559a5a7503ca995adae1f4dbcc08e599
Helin Ayça Harmancı <helin.harmanci@technarts.com>
2025-12-14 16:54:54 +0300

    Time: 54 ms (9.12%) | Memory: 18.3 MB (9.11%) - LeetSync

---
+++ Easy/6-zigzag-conversion/zigzag-conversion.py
@@ -0,0 +1,48 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        result = ""
+        string_lists = list()
+        for i in range (0, numRows):
+            st = ""
+            string_lists.append(st)
+        
+        loop_number = int(len(s)/numRows)#3
+        if loop_number < 1 or numRows==1 :
+            string_lists[0] += s
+            return string_lists[0]
+
+        string_index = 0
+        list_index = -1
+
+        for i in range (0, loop_number):
+            list_index += 1
+            print("start of going down: s")
+            while list_index < numRows:
+                if string_index == len(s):
+                    break
+                print(f"string index {string_index} is added to list index {list_index}")
+                string_lists[list_index] += s[string_index]
+
+                string_index += 1
+                list_index += 1
+            
+            
+            list_index -= 1
+            print("start of going up")
+            while list_index > 0:
+                if string_index == len(s):
+                    break
+                print(f"string index {string_index} is added to list index {list_index-1}")
+                string_lists[list_index-1] += s[string_index]
+                list_index -= 1
+                string_index += 1
+
+        for i in range (0, numRows):
+            result += string_lists[i]
+
+        return result
+
+
+        
+                
+                

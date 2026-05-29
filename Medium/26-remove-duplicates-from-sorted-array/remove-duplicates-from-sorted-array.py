bd6191a03f9db73989f12f322e915d236aadf3c1
Helin Ayça Harmancı <helin.harmanci@technarts.com>
2026-05-29 21:13:11 +0300

    Time: 3 ms (45.17%) | Memory: 20.7 MB (16.45%) - LeetSync

---
+++ Medium/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        count = 0
+        compared_to_index = 0
+        compare_index = 1
+
+        while compare_index<=len(nums)-1:
+
+            if nums[compare_index]== nums[compared_to_index]:
+                nums[compare_index] = 101
+                compare_index += 1
+                count += 1
+            else:
+                compared_to_index = compare_index
+                compare_index += 1
+        nums.sort()
+        return len(nums)-count
+
+            
+
+        
